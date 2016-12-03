75277b090b91c46a57bd15441a8bde9143cdac95
barbosaadriano <faculdadealfa@Mac-19.local>
2016-12-03 11:02:30 -0200

    Telas

---
+++ CarSaleAdriano/DetalhesViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetalhesViewController.h
+//  CarSaleAdriano
+//
+//  Created by Faculdade Alfa on 03/12/16.
+//  Copyright (c) 2016 Avantagem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetalhesViewController : UIViewController
+
+@end
---
+++ CarSaleAdriano/DetalhesViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetalhesViewController.m
+//  CarSaleAdriano
+//
+//  Created by Faculdade Alfa on 03/12/16.
+//  Copyright (c) 2016 Avantagem. All rights reserved.
+//
+
+#import "DetalhesViewController.h"
+
+@interface DetalhesViewController ()
+
+@end
+
+@implementation DetalhesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
